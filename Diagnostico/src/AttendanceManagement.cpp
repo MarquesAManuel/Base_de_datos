f2c257950084625467ef4b272544de7ffb618ea9
=MarquesAManuel <MANU_J55@live.com.ar>
2022-05-09 21:27:45 -0300

    Cambios 9 de Mayo 2022

--- Diagnostico/src/AttendanceManagement.cpp
+++ Diagnostico/src/AttendanceManagement.cpp
@@ -1,27 +1,49 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 #include "../include/attendanceManagement.h"
 
 using namespace std;
 
+AttendanceManagement::AttendanceManagement()
+{
+    
+}
 
 void AttendanceManagement::takeAttendance(Student *student, Course *course, string date, bool attendance)
 {
-    ofstream studentsAttendance;
+    ofstream studentsAttendance("StudentAttendanceList.csv");
 
-    Student _student = *student;
-    Course _course = *course;
+    if (studentsAttendance.is_open())
+    {
+        studentsAttendance << "sep=," << endl;
+        studentsAttendance << student->getIdentifier() << "," << student->getName() << ","
+        << student->getSurname() << "," << course->getIdentifier() << "," << course->getName() << " , "
+        << date << "," << attendance << "," ;
+        studentsAttendance << " ";
 
-    studentsAttendance.open("StudentAttendanceList.csv");
-
-    studentsAttendance << _student.getIdentifier() << "," << _student.getName() << "," 
-    << _student.getSurname() << "," << _course.getIdentifier() << "," << _course.getName() << ","
-    << date << "," << attendance << ",";
+        studentsAttendance.close();
+    }
+    else cout << "Unable to open file ";  
 
 }
 
 void AttendanceManagement::showAttendance()
 {
+    string line;
+    ifstream studentsAttendance("StudentAttendanceList.csv");
+    if (studentsAttendance.is_open())
+    {
+        while( getline(studentsAttendance,line))
+        {
+            cout << line << endl;
+            getchar();
+        }
+
+        studentsAttendance.close();
+    }
+
+    else cout << "Unable to open file ";
 
 }
 
